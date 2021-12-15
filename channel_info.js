83e522fa63d0e7da566b9c29bb5914d54350186b
jardondaful <68207907+jardondaful@users.noreply.github.com>
2021-12-15 15:06:20 -0800

    Update channel_info.js

--- channel_info.js
+++ channel_info.js
@@ -1,8 +1,4 @@
 export const CHANNEL_NAME = '#JardonDaFul';
 export const OAUTH_TOKEN = 'oauth:airuj8303k09une0f08euh';
 export const BOT_USERNAME = 'JardonDaFul';
-export const BLOCKED_WORDS = [
-  'faggot',
-  'cunt',
-  'nigger',
-]
+export const BLOCKED_WORDS = ['faggot','cunt','nigger',]
