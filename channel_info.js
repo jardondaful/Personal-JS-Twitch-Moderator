1ce1b695da20cfd48c39b20b7a8dbdd3d329130d
jardondaful <68207907+jardondaful@users.noreply.github.com>
2021-12-15 15:05:45 -0800

    Create channel_info.js

---
+++ channel_info.js
@@ -0,0 +1,8 @@
+export const CHANNEL_NAME = '#JardonDaFul';
+export const OAUTH_TOKEN = 'oauth:airuj8303k09une0f08euh';
+export const BOT_USERNAME = 'JardonDaFul';
+export const BLOCKED_WORDS = [
+  'faggot',
+  'cunt',
+  'nigger',
+]
