b369b49ac3bdcc707cbacec4af161f28fec21df6
jardondaful <68207907+jardondaful@users.noreply.github.com>
2021-12-15 18:04:38 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import tmi from 'tmi.js'
-import { BOT_USERNAME , OAUTH_TOKEN, CHANNEL_NAME, BLOCKED_WORDS } from './channel_info'
+import { BOT_USERNAME , OAUTH_TOKEN, CHANNEL_NAME, BLOCKED_WORDS } from './channel_info_and_banned_words'
 
 const options = 
 {
@@ -88,59 +88,68 @@ function onConnectedHandler(address, port)
   console.log(`Connected: ${address}:${port}`)
 }
 
+//Tells bot to give thanks to whoever is hosting my Twitch channel in chat
 function onHostedHandler (channel, username, viewers, autohost) 
 {
-  client.say(channel,`Thank you @${username} for the host of ${viewers}!`)
+  client.say(channel,`Thank you so much @${username} for the host of ${viewers}!`)
 }
 
+//Tells bot to give thanks to whoever is raiding my Twitch channel in chat
 function onRaidedHandler(channel, username, viewers) 
 {
-  client.say(channel,`Thank you @${username} for the raid of ${viewers}!`)
+  client.say(channel,`Thank you so much @${username} for the raid of ${viewers}!`)
 }
 
+//Tells bot to give thanks to whoever subscribed to my Twitch channel in chat
 function onSubscriptionHandler(channel, username, method, message, userstate) 
 {
-  client.say(channel,`Thank you @${username} for subscribing!`)
+  client.say(channel,`Thank you so much @${username} for subscribing!`)
 }
 
+//Tells bot to give thanks to whoever cheered bits in chat and says how many bits the viewer cheered
 function onCheerHandler(channel, userstate, message)  
 {
-  client.say(channel,`Thank you @${userstate.username} for the ${userstate.bits} bits!`)
+  client.say(channel,`Thank you so much @${userstate.username} for the ${userstate.bits} bits!`)
 }
 
+//Tells bot to give thanks to whoever subscribed to my Twitch channel in chat
 function onGiftPaidUpgradeHandler(channel, username, sender, userstate) 
 {
-  client.say(channel,`Thank you @${username} for continuing your gifted sub!`)
+  client.say(channel,`Thank you so much @${username} for continuing your gifted sub!`)
 }
 
+//Tells bot to tell viewers that my channel will be hosting another channel that is currently live
 function onHostingHandler(channel, target, viewers) 
 {
   client.say(channel,`We are now hosting ${target} with ${viewers} viewers!`)
 }
 
+//outputs that the bot is reconnecting to my channel
 function reconnectHandler() 
 {
   console.log('Reconnecting...')
 }
 
+//Tells bot to give thanks to whoever resubscribed to my Twitch channel in chat
 function resubHandler(channel, username, months, message, userstate, methods) 
 {
   const cumulativeMonths = userstate['msg-param-cumulative-months']
-  client.say(channel, `Thank you @${username} for the ${cumulativeMonths} sub!`)
+  client.say(channel, `Thank you so much @${username} for the ${cumulativeMonths} sub! You are quite the dedicated viewer!`)
 }
 
+//Tells bot to give thanks to whoever gifted a subscription to my Twitch channel to someone in chat
 function subGiftHandler(channel, username, streakMonths, recipient, methods, userstate) 
 {
-  client.say(channel,`Thank you @${username} for gifting a sub to ${recipient}}.`)
+  client.say(channel,`Thank you so much @${username} for gifting a sub to ${recipient}}.`)
 }
 
-// commands
-
+//Tells bot to greet me when I go live
 function hello(channel, userstate) 
 {
   client.say(channel, `@${userstate.username}, heya!`)
 }
 
+//Tells bot to delete a message from a viewer that contains an explicit word mentioned
 function checkTwitchChat(userstate, message, channel) 
 {
   console.log(message)
@@ -149,9 +158,9 @@ function checkTwitchChat(userstate, message, channel)
   shouldSendMessage = BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord.toLowerCase()))
   if (shouldSendMessage) 
   {
-    //tells user that his/her message was deleted
+    //tells user that his/her message was deleted when he/she sends a message that contains a banned word in channel_info_and_banned_words.js
     client.say(channel, `@${userstate.username}, sorry!  You message was deleted.`)
-    // delete message
+    //deletes the message that contains a banned word
     client.deletemessage(channel, userstate.id)
   }
 }
